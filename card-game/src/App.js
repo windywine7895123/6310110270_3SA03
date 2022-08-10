4d20fd0fb7775d3be624303f1d0a5d6132779390
Papawin Muenpatthanakit <pearlpapawin119@gmail.com>
2022-08-10 23:18:42 +0700

    7.4new align and How to play

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -9,8 +9,8 @@ const buttonRefresh = () =>
 function App() {
   return (
     <div>
-
-          <button className = 'button button1' onClick={buttonRefresh}>Refresh</button>
+          <center>
+          <marquee direction="left">{"Click alphabet cards to be word"}</marquee>
           <br></br>
           1st Word
           <WordCard value="hello"/>
@@ -20,16 +20,14 @@ function App() {
           
           3rd Word
           <WordCard value="Pearl"/>
+          <button className = 'button button1'  onClick={buttonRefresh}>Refresh</button>
           <details>
             <summary>Hint</summary>
-              <p>
                 1.The word same meaning as "Hi", (5 alphabets)<br></br>
                 2.Adjetive of your cat,(4 alphabets)<br></br>
                 3.Precious thing in the ocean,(5 alphabets)<br></br>
-
-              </p>
           </details>
-        
+          </center>
     </div>
     );
   }   
