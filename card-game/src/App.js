9d7a35dab60378ec4f3af946ada6b4f3bb50a7ab
Papawin Muenpatthanakit <pearlpapawin119@gmail.com>
2022-08-10 22:30:27 +0700

    7.1Refresh Button

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
+const buttonRefresh = () =>
+{
+    window.location.reload();
+}
 
-const word = "Hello";
 function App() {
   return (
     <div>
+        <button className = 'button button1' onClick={buttonRefresh}>Refresh</button>
         <WordCard value="hello"/>
+        <WordCard value="Nano"/>
     </div>
     );
   }   
--- card-game/src/CharacterCard.js
+++ card-game/src/CharacterCard.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
 export default function CharacterCard(props) {
- const [active, setActive] = useState(false);
- const attemptRef = useRef(props.attempt);
+const [active, setActive] = useState(false);
+const attemptRef = useRef(props.attempt);
  
- const activate = () => {
+const activate = () => {
    if(!active){
       setActive(true)
       props.activationHandler(props.value)  }
    }
 
-   useEffect(() => {
-      if(attemptRef.current != props.attempt){
-          setActive(false);
-          attemptRef.current = props.attempt
+useEffect(() => {
+   if(attemptRef.current != props.attempt){
+      setActive(false);
+      attemptRef.current = props.attempt
       }
   })
 
-   const className = `card ${active ? 'activeCard': ''}`
+const className = `card ${active ? 'activeCard': ''}`
    return (
        <div className={className} onClick={activate}>{props.value}</div>
    )
