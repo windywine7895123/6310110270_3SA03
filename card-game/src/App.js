2a88dcfaa0ae7bd1ff1c302ed567ed4fe849e27b
Papawin Muenpatthanakit <pearlpapawin119@gmail.com>
2022-08-10 23:43:20 +0700

    7.5Answer and CSS Styling

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -8,25 +8,33 @@ const buttonRefresh = () =>
 
 function App() {
   return (
-    <div>
+    <div className='background'>
           <center>
-          <marquee direction="left">{"Click alphabet cards to be word"}</marquee>
+          <marquee className ='Howto' direction="left">{"Click alphabet cards to be word"}</marquee>
           <br></br>
-          1st Word
+          <p className='text1' >1st Word</p>
           <WordCard value="hello"/>
-        
-          2nd Word
+
+          <p className='text1' >2nd Word</p>
           <WordCard value="Cute"/>
           
-          3rd Word
+          <p className='text1' >3rd Word</p>
           <WordCard value="Pearl"/>
           <button className = 'button button1'  onClick={buttonRefresh}>Refresh</button>
-          <details>
+          <details className='Hint'>
             <summary>Hint</summary>
                 1.The word same meaning as "Hi", (5 alphabets)<br></br>
                 2.Adjetive of your cat,(4 alphabets)<br></br>
                 3.Precious thing in the ocean,(5 alphabets)<br></br>
           </details>
+
+          <details className='Answer'>
+            <summary>Answer</summary>
+                1.Hello<br></br>
+                2.Cute<br></br>
+                3.Pearl<br></br>
+          </details>
+
           </center>
     </div>
     );
